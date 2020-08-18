f850dba24e013a22327f2fbe67807ececafb97e4
David Melamed <dvdmelamed@gmail.com>
2020-08-18 20:45:35 +0300

    Update baseURL.groovy

--- resources/config/groovy/baseURL.groovy
+++ resources/config/groovy/baseURL.groovy
@@ -7,5 +7,5 @@
 import jenkins.model.JenkinsLocationConfiguration
 
 JenkinsLocationConfiguration location = Jenkins.instance.getExtensionList('jenkins.model.JenkinsLocationConfiguration')[0]
-location.url = 'http://192.168.99.100:30001/' // minikube:jenkins-node-port
+location.url = 'http://localhost:8088/' // minikube:jenkins-node-port
 location.save()
